b42e4c85a781eaa93da5de896f973aa12e200faa
afif2100 <32302970+afif2100@users.noreply.github.com>
2023-02-25 23:29:26 +0700

    edit some conig

--- request-script.sh
+++ request-script.sh
@@ -1,10 +1,7 @@
 #usr/bin/bash
 
-siege -c 1 -r 10000 http://localhost:8000/
+siege -c 1 -r 10 http://localhost:8000/
 siege -c 1 -r 5 http://localhost:8000/io_task
 siege -c 1 -r 5 http://localhost:8000/cpu_task
 siege -c 1 -r 3 http://localhost:8000/random_sleep
 siege -c 1 -r 10 http://localhost:8000/random_status
-
-
-hey -c 10 -z 600s http://localhost:8000/
